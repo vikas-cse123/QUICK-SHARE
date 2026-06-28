88b647d4518b53019734c49e9829bee973b7effc
Akash <akash@Akashs-Mac-mini.local>
2026-06-29 02:37:12 +0530

    Add category endpoint

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 import express from "express"
+import { connectDb } from "./config/db.js"
 const app = express()
 const PORT = process.env.PORT
 
+await connectDb()
 
 
 
 
+
+
+app.get("/categories",(req,res) => {
+    const categories = ["None","Cryptocurrency","Cybersecurity","Fixit","Food","Gaming","Haike","Help","History","Housing","Jokes","Legal",'Money','Movies',"Music","Pets","Photo","Science","Software","Source Code","Spirit","Sports","Travel","TV","Writing"]
+    res.status(200).json(categories)
+    
+})
 
 
 
